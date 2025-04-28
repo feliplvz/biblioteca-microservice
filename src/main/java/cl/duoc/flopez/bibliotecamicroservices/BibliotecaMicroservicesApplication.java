8a629593fdcd9c2408770c5cc95021529eb64e69
Felipe López Valenzuela <felipelopez.valenzuela@gmail.com>
2025-04-28 19:28:10 -0400

    Iniciamos

---
+++ src/main/java/cl/duoc/flopez/bibliotecamicroservices/BibliotecaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.flopez.bibliotecamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/duoc/flopez/bibliotecamicroservices/BibliotecaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.flopez.bibliotecamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
